dda0b37d5bc57204d4f8a5cd23178855ddd4024c
Alistair Edmunds <alistair.edmunds@gmail.com>
2013-08-08 11:10:09 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RenewLegal::Application.config.secret_key_base = 'ec121a837e2c6e42e1fb005683eb71ffe8ff8b848462176e460b8bad3638df54b81f6fc857f91cf168853d6d524f65948277772edb518e74ca32f7e26615ef13'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RenewLegal::Application.config.session_store :cookie_store, key: '_renew_legal_session'
